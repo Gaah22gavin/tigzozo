c72a62d2afe921c4bf5589aa785f69612954715f
Gaah22gavin <89858687+Gaah22gavin@users.noreply.github.com>
2022-05-22 12:03:07 +0200

    second

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Header} from './header/Header';
-export {default as Banner} from './banner/Banner';
-export {default as AboutUs} from './aboutUs/AboutUs';
-export {default as Services} from './services/Services';
-export {default as Clients} from './clients/Clients';
-export {default as Instagram} from './instagram/Instagram';
-export {default as Footer} from './footer/Footer';
